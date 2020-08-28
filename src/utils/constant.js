b5a208582d2ab33d1b0767a00eab1a68645d82e9
Gian <giand2205@gmail.com>
2020-08-28 08:29:41 -0500

    fix url heroky - redirect for netlify

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,3 @@
-export const API_HOST = "http://localhost:8080";
+//export const API_HOST = "http://localhost:8080";
+export const API_HOST = "https://twittor-gw.herokuapp.com";
 export const TOKEN = "token";
